fa124c696fabb28cedf676b3ae4331ed1a41b298
Mehmet Yaman <mehmetyaman331@gmail.com>
2021-09-06 16:43:24 +0300

    addOrder function firebase integration

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
 import './cart.dart';
 
 class OrderItem {
@@ -23,11 +26,29 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final url = Uri.https(
+        'shop-app-flutter-b2ba6-default-rtdb.firebaseio.com', '/orders.json');
+    final timestamp = DateTime.now();
+    final response = await http.post(
+      url,
+      body: json.encode({
+        'amount': total,
+        'dateTime': timestamp.toIso8601String(),
+        'products': cartProducts
+            .map((cp) => {
+                  'id': cp.id,
+                  'title': cp.title,
+                  'quantity': cp.quantity,
+                  'price': cp.price,
+                })
+            .toList(),
+      }),
+    );
     _orders.insert(
       0,
       OrderItem(
-        id: DateTime.now().toString(),
+        id: json.decode(response.body)['name'],
         amount: total,
         dateTime: DateTime.now(),
         products: cartProducts,
