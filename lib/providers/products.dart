eab7c5c3a9474a7d97df5e9f153756e5cbe1fd75
Mehmet Yaman <mehmetyaman331@gmail.com>
2021-09-06 14:00:29 +0300

    firebase link fixed

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -69,8 +69,8 @@ class Products with ChangeNotifier {
   // }
 
   Future<void> fetchAndSetProducts() async {
-    final url = Uri.https('shop-app-flutter-b2ba6-default-rtdb.firebaseio.com/',
-        '/products.json');
+    final url = Uri.https(
+        'shop-app-flutter-b2ba6-default-rtdb.firebaseio.com', '/products.json');
     try {
       final response = await http.get(url);
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
@@ -95,8 +95,7 @@ class Products with ChangeNotifier {
 
   Future<void> addProduct(Product product) async {
     final url = Uri.https(
-        'shop-app-flutter-b2ba6-default-rtdb.firebaseio.com/products',
-        '/products.json');
+        'shop-app-flutter-b2ba6-default-rtdb.firebaseio.com', '/products.json');
 
     try {
       final response = await http.post(
@@ -125,9 +124,18 @@ class Products with ChangeNotifier {
     }
   }
 
-  void updateProduct(String id, Product newProduct) {
+  Future<void> updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
+      final url = Uri.https(
+          'shop-app-flutter-b2ba6-default-rtdb.firebaseio.com',
+          '/products/$id.json');
+      await http.patch(url, body: {
+        "title": newProduct.title,
+        "description": newProduct.description,
+        "imageUrl": newProduct.imageUrl,
+        "price": newProduct.price,
+      });
       _items[prodIndex] = newProduct;
       notifyListeners();
     } else {
